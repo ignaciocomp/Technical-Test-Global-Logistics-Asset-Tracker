ea2ac05e09ddcf5978176c3cde03792b2459fb7f
ignaciocomp <your-email@example.com>
2026-03-03 14:55:46 -0300

    feat: API layer - controllers, JWT auth, middleware and swagger

--- src/GLAT.Application/DependencyInjection.cs
+++ src/GLAT.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using FluentValidation;
+using GLAT.Application.Common.Behaviors;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GLAT.Application;
@@ -10,6 +12,7 @@ public static class DependencyInjection
         var assembly = typeof(DependencyInjection).Assembly;
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
         services.AddValidatorsFromAssembly(assembly);
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         return services;
     }
 }
